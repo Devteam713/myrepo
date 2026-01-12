04093031c635327640b47ea56238daa555a560bb
Dev713 <53387865+erikg713@users.noreply.github.com>
2026-01-12 12:43:58 -0600

    Update semantic_engine.py

--- Sentenial-X: Alethia/core/semantic_plane/semantic_engine.py
+++ Sentenial-X: Alethia/core/semantic_plane/semantic_engine.py
@@ -1,3 +1,22 @@
+from core.semantic_plane.semantic_decay import evaluate_state
+from core.linguistics.entropy_ops import apply_entropy
+
+class SemanticEngine:
+    """
+    Executes meaning as a function of trust and entropy.
+    """
+
+    def execute(self, ses):
+        ses.semantic_coherence, ses.execution_state = evaluate_state(ses)
+
+        if ses.execution_state != "resolved":
+            ses.semantic_payload = apply_entropy(
+                ses.semantic_payload,
+                ses.entropy_budget
+            )
+        return ses
+
+
 from core.linguistics.entropy_ops import apply_entropy
 
 
