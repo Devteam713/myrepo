6d7278bfed0da36627e7f783e6cb13169e66b5d7
Dev713 <53387865+erikg713@users.noreply.github.com>
2026-01-12 12:36:15 -0600

    Update semantic_engine.py

--- Sentenial-X: Alethia/core/semantic_plane/semantic_engine.py
+++ Sentenial-X: Alethia/core/semantic_plane/semantic_engine.py
@@ -1,1 +1,18 @@
+from core.linguistics.entropy_ops import apply_entropy
 
+
+class SemanticEngine:
+    """
+    Executes meaning.
+    """
+
+    def execute(self, ses):
+        ses.evaluate()
+
+        if ses.state != "resolved":
+            ses.payload = apply_entropy(
+                ses.payload,
+                ses.entropy_budget
+            )
+
+        return ses
