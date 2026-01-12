3afeac1b7ff1e502f8f35d9bdda9e667a58b53a6
Dev713 <53387865+erikg713@users.noreply.github.com>
2026-01-12 12:47:33 -0600

    Update entropy_ops.py

--- Sentenial-X: Alethia/core/linguistics/entropy_ops.py
+++ Sentenial-X: Alethia/core/linguistics/entropy_ops.py
@@ -1,1 +1,11 @@
+import random
 
+def apply_entropy(text: str, level: float) -> str:
+    words = text.split()
+    swaps = int(len(words) * level)
+
+    for _ in range(swaps):
+        i, j = random.sample(range(len(words)), 2)
+        words[i], words[j] = words[j], words[i]
+
+    return " ".join(words)
