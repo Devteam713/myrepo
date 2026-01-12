62bb5edfe1d90fdd185bc1e4b7d290318cc946ab
Dev713 <53387865+erikg713@users.noreply.github.com>
2026-01-12 12:11:36 -0600

    Update linguistic_entropy.py

--- Sentenial-X: Alethia/core/entropy/linguistic_entropy.py
+++ Sentenial-X: Alethia/core/entropy/linguistic_entropy.py
@@ -1,1 +1,20 @@
+import random
 
+
+class LinguisticEntropyEngine:
+    """
+    Applies controlled linguistic instability.
+    """
+
+    def apply(self, text: str, entropy_level: float) -> str:
+        if entropy_level <= 0:
+            return text
+
+        words = text.split()
+        swaps = int(len(words) * entropy_level)
+
+        for _ in range(swaps):
+            i, j = random.sample(range(len(words)), 2)
+            words[i], words[j] = words[j], words[i]
+
+        return " ".join(words)
