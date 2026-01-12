d9573e98c32d5acbee748b87d2255c9132a4031a
Dev713 <53387865+erikg713@users.noreply.github.com>
2026-01-12 12:48:40 -0600

    Update alethia_runtime.py

--- Sentenial-X: Alethia/core/orchestration/alethia_runtime.py
+++ Sentenial-X: Alethia/core/orchestration/alethia_runtime.py
@@ -1,1 +1,14 @@
+from core.semantic_plane.semantic_engine import SemanticEngine
+from core.semantic_plane.semantic_policies import entropy_from_trust
 
+class AlethiaRuntime:
+    """
+    Single integration entrypoint for Sentenial-X.
+    """
+
+    def __init__(self):
+        self.engine = SemanticEngine()
+
+    def render(self, ses, trust_score):
+        ses.entropy_budget = entropy_from_trust(trust_score)
+        return self.engine.execute(ses)
