8f09552c5327c20d0dffc6f69ef6ffa6ace4a15e
Dev713 <53387865+erikg713@users.noreply.github.com>
2026-01-12 12:13:37 -0600

    Update semantic_router.py

--- Sentenial-X: Alethia/core/orchestration/semantic_router.py
+++ Sentenial-X: Alethia/core/orchestration/semantic_router.py
@@ -1,1 +1,16 @@
+from core.entropy.entropy_engine import EntropyEngine
+from core.resolution.resolution_engine import ResolutionEngine
 
+
+class SemanticRouter:
+    """
+    Central semantic execution path.
+    """
+
+    def __init__(self):
+        self.entropy_engine = EntropyEngine()
+        self.resolution_engine = ResolutionEngine()
+
+    def process(self, semantic_state):
+        semantic_state = self.entropy_engine.process(semantic_state)
+        return self.resolution_engine.resolve(semantic_state)
