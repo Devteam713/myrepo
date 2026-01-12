103d6a729ef827225bd40bc6ffbde0c4ef2b2a16
Dev713 <53387865+erikg713@users.noreply.github.com>
2026-01-12 12:12:21 -0600

    Update entropy_engine.py

--- Sentenial-X: Alethia/core/entropy/entropy_engine.py
+++ Sentenial-X: Alethia/core/entropy/entropy_engine.py
@@ -1,1 +1,18 @@
+from core.entropy.linguistic_entropy import LinguisticEntropyEngine
 
+
+class EntropyEngine:
+    """
+    Decides how much entropy to apply.
+    """
+
+    def __init__(self):
+        self.les = LinguisticEntropyEngine()
+
+    def process(self, semantic_state):
+        entropy = semantic_state.entropy_level
+        semantic_state.semantic_payload = self.les.apply(
+            semantic_state.semantic_payload,
+            entropy
+        )
+        return semantic_state
